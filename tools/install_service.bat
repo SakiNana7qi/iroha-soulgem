a6c714fd0d4dce018766563a92535d1bd0bacd91
SakiNana7qi <1632466445@qq.com>
2026-05-05 03:48:43 +0800

    fix: 更新服务安装脚本

--- tools/install_service.bat
+++ tools/install_service.bat
@@ -3,8 +3,8 @@ setlocal
 
 set SERVICE_NAME=SoulGemMonitor
 set SERVICE_DISPLAY=SoulGem Server Monitor
-set SCRIPT_DIR=%~dp0
-set SCRIPT_PATH=%SCRIPT_DIR%main.py
+set SCRIPT_DIR=%~dp0..
+set SCRIPT_PATH=%SCRIPT_DIR%\main.py
 
 for /f "delims=" %%i in ('where python') do (
     set PYTHON_EXE=%%i
@@ -23,22 +23,30 @@ if %errorlevel% neq 0 (
     exit /b 1
 )
 
+echo.
+echo Service will run as current user: %USERDOMAIN%\%USERNAME%
+set /p SVC_PASSWORD=Enter password for %USERDOMAIN%\%USERNAME%:
+
+echo.
 echo Installing service: %SERVICE_NAME%
 nssm install %SERVICE_NAME% "%PYTHON_EXE%" "%SCRIPT_PATH%"
 nssm set %SERVICE_NAME% DisplayName "%SERVICE_DISPLAY%"
-nssm set %SERVICE_NAME% AppDirectory "%SCRIPT_DIR:~0,-1%"
+nssm set %SERVICE_NAME% AppDirectory "%SCRIPT_DIR%"
 nssm set %SERVICE_NAME% Start SERVICE_AUTO_START
-nssm set %SERVICE_NAME% AppStdout "%SCRIPT_DIR%logs\service.log"
-nssm set %SERVICE_NAME% AppStderr "%SCRIPT_DIR%logs\service.log"
+nssm set %SERVICE_NAME% AppStdout "%SCRIPT_DIR%\logs\service.log"
+nssm set %SERVICE_NAME% AppStderr "%SCRIPT_DIR%\logs\service.log"
 nssm set %SERVICE_NAME% AppRotateFiles 1
 nssm set %SERVICE_NAME% AppRotateBytes 10485760
+nssm set %SERVICE_NAME% ObjectName "%USERDOMAIN%\%USERNAME%" "%SVC_PASSWORD%"
 
-if not exist "%SCRIPT_DIR%logs" mkdir "%SCRIPT_DIR%logs"
+if not exist "%SCRIPT_DIR%\logs" mkdir "%SCRIPT_DIR%\logs"
 
 echo Starting service...
 nssm start %SERVICE_NAME%
 
+echo.
 echo Done. Service "%SERVICE_DISPLAY%" installed and started.
-echo Logs: %SCRIPT_DIR%logs\service.log
+echo Running as: %USERDOMAIN%\%USERNAME%
+echo Logs: %SCRIPT_DIR%\logs\service.log
 
 pause
